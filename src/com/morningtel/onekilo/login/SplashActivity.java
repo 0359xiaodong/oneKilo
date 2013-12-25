066ab13f3c87699e98cb5da2ce6d68f3778011c3
renyu <Yuren2@hotmail.com>
2013-12-25 08:36:47 +0800

    百度定位sdk不显示bug修复

--- src/com/morningtel/onekilo/login/SplashActivity.java
+++ src/com/morningtel/onekilo/login/SplashActivity.java
@@ -1,15 +1,21 @@
 package com.morningtel.onekilo.login;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Window;
 
-import cn.jpush.android.api.InstrumentedActivity;
-
+import com.baidu.mobstat.StatService;
 import com.morningtel.onekilo.OneKiloApplication;
 import com.morningtel.onekilo.R;
+import com.morningtel.onekilo.common.CommonUtils;
+import com.morningtel.onekilo.common.Conn;
+import com.morningtel.onekilo.main.MainActivity;
+import com.morningtel.onekilo.model.User;
 
-public class SplashActivity extends InstrumentedActivity {
+public class SplashActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +24,51 @@ public class SplashActivity extends InstrumentedActivity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
 		
-		if(((OneKiloApplication) getApplication()).isAppOpen) {
-			System.out.println("已经有了实例了");
-			finish();
-		}
-		else {
-			Intent intent=new Intent(this, UserControllerActivity.class);
-			startActivity(intent);
-			finish();
-		}
+		handler.sendEmptyMessageDelayed(0, 3000);
 	}
 
+	public void onResume() {
+		super.onResume();
+
+		/**
+		 * 页面起始（每个Activity中都需要添加，如果有继承的父Activity中已经添加了该调用，那么子Activity中务必不能添加）
+		 * 不能与StatService.onPageStart一级onPageEnd函数交叉使用
+		 */
+		StatService.onResume(this);
+	}
+
+	public void onPause() {
+		super.onPause();
+
+		/**
+		 * 页面结束（每个Activity中都需要添加，如果有继承的父Activity中已经添加了该调用，那么子Activity中务必不能添加）
+		 * 不能与StatService.onPageStart一级onPageEnd函数交叉使用
+		 */
+		StatService.onPause(this);
+	}
+	
+	Handler handler=new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+			if(((OneKiloApplication) getApplication()).isAppOpen) {
+				System.out.println("已经有了实例了");
+				finish();
+			}
+			else {
+				Intent intent=null;
+				User user=Conn.getInstance(SplashActivity.this).getLoginUser();
+				if(user!=null&&CommonUtils.isAlreadyLoginOut(SplashActivity.this, user.getToken())) {
+					((OneKiloApplication) getApplicationContext()).user=user;
+					intent=new Intent(SplashActivity.this, MainActivity.class);
+				}
+				else {
+					intent=new Intent(SplashActivity.this, LoginActivity.class);
+				}
+				startActivity(intent);
+				finish();
+			}
+		}
+	};
 }
